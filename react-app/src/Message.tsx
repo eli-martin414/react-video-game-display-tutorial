53e70d1a07e2bfc9e71d46c075623739fc748ad5
eli-martin414 <70294157+eli-martin414@users.noreply.github.com>
2023-08-01 23:16:20 -0500

    change Message to display name

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,15 @@
 // use PascalCasing when creating a function-based component
 function Message() {
-    // This is JSX (JavaScript XML)
+    // This is JSX (JavaScript XML): return <h1>Hello world</h1>;
     // it gets converted to something like React.createElement("h1", null, "Hello World");
+    // We can combine it with normal JS using {}
+
+    const name = 'Eli';
+
+    // This will show either "Hello [name]"
+    // or "Hello World" if name = ''
+    if (name)
+        return <h1>Hello {name}</h1>;
     return <h1>Hello world</h1>;
 }
 
