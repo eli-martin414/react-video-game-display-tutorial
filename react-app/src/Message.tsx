383b978f7ca2d0ea899d3e83317917b08837eda9
eli-martin414 <70294157+eli-martin414@users.noreply.github.com>
2023-08-01 23:12:18 -0500

    add Hello World message component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+// use PascalCasing when creating a function-based component
+function Message() {
+    // This is JSX (JavaScript XML)
+    // it gets converted to something like React.createElement("h1", null, "Hello World");
+    return <h1>Hello world</h1>;
+}
+
+export default Message;
