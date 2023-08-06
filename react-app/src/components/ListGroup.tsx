f125f4828782f3935fea269b955684153853c096
eli-martin414 <70294157+eli-martin414@users.noreply.github.com>
2023-08-05 23:59:27 -0500

    Conditional Rendering example

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,33 @@
 function ListGroup() {
+  let items = ["NYC", "San Fran", "Tokyo", "Paris", "London"];
+  items = [];
+
+  // const message = items.length === 0 ? <p>NO item found</p> : null;
+  // same result as using function below, just put {message} in JSX where we want it
+  // const is better if it's actually constant; but func can be good if you need parameters
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>NO item found</p> : null;
+  };
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found.</p>
+      </>
+    );
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
